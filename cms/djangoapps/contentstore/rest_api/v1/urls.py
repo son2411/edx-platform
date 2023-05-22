135befa10d5e0d11e77730a127688ef2a3e7d0cf
Jesper Hodge <jhodge@outlook.de>
2023-05-22 19:24:43 +0000

    refactor: create /views folder

--- cms/djangoapps/contentstore/rest_api/v1/urls.py
+++ cms/djangoapps/contentstore/rest_api/v1/urls.py
@@ -4,14 +4,14 @@ from django.urls import re_path
 
 from openedx.core.constants import COURSE_ID_PATTERN
 
-from . import views
+from .views import proctored_exam_settings
 
 app_name = 'v1'
 
 urlpatterns = [
     re_path(
         fr'^proctored_exam_settings/{COURSE_ID_PATTERN}$',
-        views.ProctoredExamSettingsView.as_view(),
+        proctored_exam_settings.ProctoredExamSettingsView.as_view(),
         name="proctored_exam_settings"
     ),
 ]
